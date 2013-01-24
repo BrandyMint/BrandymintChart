7a1bbffa51f380555f1bf6cc4d94b6d22159a94c
denisdbv <denisdbv@gmail.com>
2013-01-24 12:04:32 +0400

    Init app

---
+++ BrandymintChart/BrandymintChart/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  BrandymintChart
+//
+//  Created by denisdbv@gmail.com on 24.01.13.
+//  Copyright (c) 2013 denisdbv@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ BrandymintChart/BrandymintChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BrandymintChart
+//
+//  Created by denisdbv@gmail.com on 24.01.13.
+//  Copyright (c) 2013 denisdbv@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
